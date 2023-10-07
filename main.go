3fc8b4d6cc6d76c344c25393865c2be5625c8354
moyunie <13256735690@sina.cn>
2023-10-07 10:58:48 +0800

    zxuixin

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	//创建一个服务
+	ginServer := gin.Default()
+
+	//访问地址，处理我们的请求 Request Response
+	//gin.Context，封装了request和response
+	ginServer.GET("/hello", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"msg": "hello world",
+		})
+	})
+	//服务器端口
+	//Run("里面不指定端口号默认为8080")
+	ginServer.Run(":8081")
 }
